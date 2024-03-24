1e7714755d6ca1f5e95bf6ae175faeec521804f2
Farukh <excommunicad00@yandex.ru>
2024-03-24 15:10:45 +0600

    пытаюсь пройти чек билд

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -1,14 +1,16 @@
 package ru.practicum.shareit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItApp {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ShareItApp.class, args);
-        System.out.println("Java shareit");
+        System.out.println("Java shareit running on the port 8080");
     }
 
 }
