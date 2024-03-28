6bd2996154a1127447a1d5bf41cc4d0d93e10e22
Farukh <excommunicad00@yandex.ru>
2024-03-29 04:18:03 +0600

    fix checkstyle

--- src/main/java/ru/practicum/shareit/user/UserConstant.java
+++ src/main/java/ru/practicum/shareit/user/UserConstant.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class UserConstant {
     public static final String REGEX_LOGIN = "^\\S*$";
-    public static final String USER_REPOSITORY_IMPL = "я люблю нюхать бебру";
+    public static final String USER_REPOSITORY_IMPL = "USER_REPOSITORY_IMPL";
     public static final String REGEX_EMAIL = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
     public static final String IN_MEMORY_USER_REPOSITORY = "inMemoryUserRepository";
 }
