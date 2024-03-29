11bb68fd7fd98fd7347414d784ddeba950fd2cad
Farukh <excommunicad00@yandex.ru>
2024-03-29 17:44:35 +0600

    fix build application 3

--- src/main/java/ru/practicum/shareit/exception/HandlerException.java
+++ src/main/java/ru/practicum/shareit/exception/HandlerException.java
@@ -1,11 +1,9 @@
 package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.mapping.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
